90d2a97c088aa618342b285141c0d54c0715b9a0
Jimmy <itsjimmyh@gmail.com>
2015-04-14 15:00:48 -0700

    add seeds.rb to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+## Test User
+User.create( email: "asdf@gmail.com",
+             password: "asdfasdf",
+             first_name: "Test",
+             last_name: "User"
+            )
